327b948c92a32313d84ac8cde04449c83136f6c1
Miha <miha.kovac00@gmail.com>
2018-12-25 14:16:45 +0100

    committing version 2.0.0

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # time-difference
-This is a piece of code that converts timestamps (sql) to a more user friendly format like "2 days ago" or "3 weeks ago"
+This is a piece of code that converts timestamps (sql) to a more user friendly format like "2 days ago" or "3 weeks ago".
+The SQL timestamp should be in UTC time to get the proper result. Difference between user's local time zone and UTC is compensated for by adding/subtracting the timezone difference between the two.
+If the timestamp happened after midnight, the output will be either "X hours ago", "Y minutes ago" or "Z seconds ago".
+If it happened before midnight, expect "A days ago", "B weeks ago", "C months ago" or " D years ago".
+Normal behaviour (all timestamps in UTC):
+    1. The event happened yesterday at 23:15. Now it is the following day and it is 1:10 in the morning. Program will return 'Yesterday' even though the event happened less than 2 hours ago.
+    2. The event happened today at 2:40 in the morning, now it is 14:10. Program will return '11 hours ago' (It happened today).
 
+_________________________________________________________________________________________________________________________
+Version 2.0.0 commited on Dec 25 2018;
+For loops have been replaced by modulos(%), the process should now be quicker and easier to read, understand and maintain.
+OOP was replaced by functional programming (from a Class to standalone functions). This gives better compatibility with older browsers when `let` and `const` are replaced by `var`.
+There are still aproximations (month == 30days, year == 365days), therefore the program is 100% correct. By attempting to fix that, the program would become a lot longer and less readable. That is the reason I decided not to fix that. Feel free to add that yourself and to send a merge request so everybody can benifit from that.
+Refer to the example if you have difficulties setting it up.
+
+_________________________________________________________________________________________________________________________
 Version 1.0.0 commited on Nov 18 2018:
 This is tested and working on Safari, Mozilla and Chrome browsers.
 Input string should be in UTC time, the script will automatically compare it to the user's time no matter the timezone.
 Feel free to look at examles.
 
-I will be further developing this code to make it faster, cleaner and hopefully more accurate.
-Version 1.1 will give an ES6 alternative to those who can afford not to support old browsers.
-Version 2 is in stage of design, for loops will be ditched in favor of modulos(%).
 
 Feel free to copy code to your project and use it.
--- examples/main.js
+++ examples/main.js
@@ -1,161 +1,110 @@
 window.onload = function () {
-    const timestamp = "2018-11-13 23:59:59"; //this is a UTC timestamp
-    const TandD = new TimeDifference(timestamp);
-    document.getElementById('mainDiv').innerHTML = TandD.returnCalculation();
+    const timestamp = "2018-12-24 23:59:59"; //this is a UTC timestamp
+    document.getElementById('mainDiv').innerHTML = converSQLtoAgoFormat(timestamp);
 }
 
-class TimeDifference {
-    constructor(a_sqlTimestamp) { //expected: "YYYY-MM-DD HH:MM:SS"
-        //LOCAL variables
-        var currTime = new Date();
-        var timezoneOffset = currTime.getTimezoneOffset() * 60000;
 
-        //had some complications with UTC (different behaviour) on Mozilla
-        //Mozilla auto-converts the string given via `new Date()` to UTC time
-        //Chrome and Safari don't; they keep it in local time
-        //tested and working on Safari, Mozilla and Chrome browsers
-        if (navigator.userAgent.search("Firefox") >= 0) {
-            var currUtcTime = new Date();
-            var currLocalTime = new Date(currTime.getTime() - timezoneOffset);
-            var timestamp = new Date(new Date(a_sqlTimestamp).getTime() - timezoneOffset);
-            var midnightLocalTime = new Date(new Date(currLocalTime.getFullYear(),
-                currLocalTime.getMonth(),
-                currLocalTime.getDate()).getTime() - timezoneOffset);
-        } else {
-            //need to convert sql timestamp to Date js object
-            var t = String(a_sqlTimestamp).split(/[- :]/);
-            var timestamp = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
-            //months are between 0 and 11 ->subtract 1
-            var currUtcTime = new Date(currTime.getTime() + timezoneOffset);
-            var currLocalTime = new Date();
-            var midnightLocalTime = new Date(currLocalTime.getFullYear(),
-                currLocalTime.getMonth(),
-                currLocalTime.getDate());
-        }
+function evaluateHMS(timeDiff) {
+    const hrsMinSec = [Math.floor(timeDiff/60/60), Math.floor(timeDiff/60), timeDiff % 60];
+    const outputs = ['hour', 'minute', 'second'];
+    const validOutput = {
+        value: 0,
+        index: 2
+    };
 
-        var milisSinceMidnight = currLocalTime.getTime() - midnightLocalTime.getTime();
-
-        //CLASS variables
-        this.currentTime = Math.floor(currUtcTime.getTime() / 1000);
-        this.midnightTime = Math.floor((currUtcTime.getTime() - milisSinceMidnight) / 1000);
-        this.timestamp = Math.floor(timestamp.getTime() / 1000);
-        this.outputStrIndex = 0;
-        this.outputIndex = '';
-        this.agoForm = false;
-        this.pluralForm = false;
-        this.isValid = true; //timestamp is not in the future
-
-        this.secondsInDay = 24 * 60 * 60;
-        this.secondsInWeek = this.secondsInDay * 7;
-        this.secondsInMonth = this.secondsInDay * 30; //approximation
-        this.secondsInYear = this.secondsInDay * 365; //approximation
-    }
-
-    moreThan1YearAgo() {
-        return ((this.currentTime - this.timestamp) > this.secondsInYear);
-    }
-    moreThan1MonthAgo() {
-        return ((this.currentTime - this.timestamp) > this.secondsInWeek * 4); //approximation to simplify
-    }
-    moreThan1WeekAgo() {
-        return ((this.currentTime - this.timestamp) > this.secondsInWeek);
-    }
-
-    checkIfValid() { //evaluates to false if the timestamp is in future
-        this.isValid = (this.currentTime >= this.timestamp);
-        return this.isValid;
-    }
-
-    dateIsToday() {
-        var seconds = (this.currentTime - this.timestamp);
-        // [seconds, minutes, hours]
-        var secMinHrs = [seconds, Math.floor(seconds / 60), Math.floor(seconds / 3600)];
-        //if hours are not 0 their index; else if minutes are not 0 their; else number of seconds
-        var index = (secMinHrs[2] ? 2 : (secMinHrs[1] ? 1 : 0))
-
-        this.outputStrIndex = index + 6;
-        this.outputIndex = String(secMinHrs[index]) + ' ';
-        this.agoForm = true;
-        this.pluralForm = (secMinHrs[index] != 1);
-    }
-
-    calculate() {
-        if (!this.checkIfValid()) {} else if (!this.moreThan1WeekAgo()) {
-            if (this.midnightTime <= this.timestamp) {
-                this.dateIsToday();
-                //this.outputStrIndex = 0; //'today'
-                return;
-            } else if ((this.midnightTime - this.secondsInDay) <= this.timestamp) {
-                this.outputStrIndex = 1; //'yesterday'
-                return;
-            }
-            for (var day = 2; day < 8; day++) {
-                if ((this.midnightTime - this.secondsInDay * day) <= this.timestamp) {
-                    this.outputStrIndex = 2; //'day'
-                    this.outputIndex = String(day) + ' ';
-                    this.agoForm = true;
-                    this.pluralForm = true;
-                    break;
-                }
-                continue;
-            }
-            return;
-        } else if (!this.moreThan1MonthAgo()) {
-            for (var week = 2; week < 5; week++) {
-                if ((this.midnightTime - this.secondsInWeek * week) <= this.timestamp) {
-                    this.outputStrIndex = 3; //'week'
-                    this.outputIndex = String(week - 1) + ' ';
-                    this.agoForm = true;
-                    this.pluralForm = (week != 2);
-                    break;
-                }
-                continue;
-            }
-            return;
-        } else {
-            if (!this.moreThan1YearAgo()) {
-                var yearException = true; //because of approximation
-                for (var month = 2; month < 13; month++) {
-                    if ((this.midnightTime - this.secondsInMonth * month) <= this.timestamp) {
-                        this.outputStrIndex = 4; //'month'
-                        this.outputIndex = String(month - 1) + ' ';
-                        this.agoForm = true;
-                        this.pluralForm = (month != 2);
-                        yearException = false;
-                        break;
-                    }
-                    continue;
-                }
-                if (yearException) {
-                    this.outputStrIndex = 5; //'year'
-                    this.outputIndex = '1 ';
-                    this.agoForm = true;
-                }
-                return;
-            }
-            for (var year = 2; year < 10; year++) //inaccuracy when 366 days in a year
-            {
-                if ((this.midnightTime - this.secondsInYear * year) <= this.timestamp) {
-                    this.outputStrIndex = 5; //'year'
-                    this.outputIndex = String(year - 1) + ' ';
-                    this.agoForm = true;
-                    this.pluralForm = (year != 2);
-                    break;
-                }
-                continue;
-            }
-            return;
+    for (let i = 0; i < hrsMinSec.length; i++) {
+        if (hrsMinSec[i]) {
+            validOutput.index = i;
+            validOutput.value = hrsMinSec[i];
+            break;
         }
     }
+    const pluralSuffix = (validOutput.value == 1) ? '' : 's';
+    return validOutput.value + ' ' + outputs[validOutput.index] + pluralSuffix + ' ago';
+}
 
-    returnCalculation() {
-        var outputs = ['Today', 'Yesterday', 'day', 'week', 'month', 'year', 'second', 'minute', 'hour'];
-        this.calculate();
-        var pluralSuffix = this.pluralForm ? 's' : '';
-        var agoSuffix = this.agoForm ? ' ago' : '';
+function evaluateYMWD(timeDiff) {
+    const yrsMonWekDay = [Math.floor(timeDiff/60/60/24/365),
+        Math.floor(timeDiff/60/60/24/30),
+        Math.floor(timeDiff/60/60/24/7),
+        Math.floor(timeDiff/60/60/24)];
+    const outputs = ['year', 'month', 'week', 'day'];
+    const validOutput = {
+        value: 0,
+        index: 3
+    };
 
-        return (this.isValid ?
-            (this.outputIndex + outputs[this.outputStrIndex] + pluralSuffix + agoSuffix) :
-            'The given timestamp is in the future!');
+    for (let i = 0; i < yrsMonWekDay.length; i++) {
+        if (yrsMonWekDay[i]) {
+            validOutput.index = i;
+            validOutput.value = yrsMonWekDay[i];
+            break;
+        }
+    }
+    const pluralSuffix = (validOutput.value == 1) ? '' : 's';
+
+    if (validOutput.index == 3 && validOutput.value == 1) {
+        return 'Yesterday';
+    }
+    return validOutput.value + ' ' + outputs[validOutput.index] + pluralSuffix + ' ago';
+}
+
+function converSQLtoAgoFormat(a_sqlTimestamp) {
+    //utc timestamp is expected as an input 'YYYY-MM-DD HH:MM:SS'
+    const TIME = {
+        dateTimeObject: new Date(),
+        timezoneOffset: 0,
+        utcTime: new Date(),
+        localTime: new Date(),
+        timestamp: 0,
+        midnightLocalTime: 0,
+        millisecondsSinceMidnight: 0
+    }
+
+    TIME.timezoneOffset = TIME.dateTimeObject.getTimezoneOffset() * 60000;
+
+    //had some complications with UTC (different behaviour) on Mozilla
+    //Mozilla auto-converts the string given via `new Date()` to UTC time
+    //Chrome and Safari don't; they keep it in local time
+    //tested and working on Safari, Mozilla and Chrome browsers
+    if (navigator.userAgent.search("Firefox") >= 0) {
+        //utc time is the time returned by `new Date();` (on Mozilla)
+        TIME.localTime = new Date(TIME.dateTimeObject.getTime() - TIME.timezoneOffset);
+        TIME.timestamp = new Date(new Date(a_sqlTimestamp).getTime() - TIME.timezoneOffset);
+        TIME.midnightLocalTime = new Date(
+            new Date(TIME.localTime.getFullYear(),
+                TIME.localTime.getMonth(),
+                TIME.localTime.getDate())
+            .getTime() - TIME.timezoneOffset);
+    } else {
+        //need to convert sql timestamp to Date js object
+        const t = String(a_sqlTimestamp).split(/[- :]/);
+        TIME.timestamp = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+        //months are between 0 and 11 -> subtract 1
+        TIME.utcTime = new Date(TIME.dateTimeObject.getTime() + TIME.timezoneOffset);
+        //local time is the time returned by `new Date();` (everywhere except on Mozilla)
+        TIME.localTime = new Date();
+        TIME.midnightLocalTime = new Date(
+            TIME.localTime.getFullYear(),
+            TIME.localTime.getMonth(),
+            TIME.localTime.getDate());
+    }
+
+    TIME.millisecondsSinceMidnight = TIME.localTime.getTime() - TIME.midnightLocalTime.getTime();
+
+    const currentUtcTimeInSeconds = Math.floor(TIME.utcTime.getTime() / 1000);
+    const midnightTimeInSeconds = Math.floor((TIME.utcTime.getTime() - TIME.millisecondsSinceMidnight) / 1000);
+    const timestampInSeconds = Math.floor(TIME.timestamp.getTime() / 1000);
+
+    const timeDifference = currentUtcTimeInSeconds - timestampInSeconds;
+
+    if (timeDifference < 0) {
+        return 'The given timestamp is in the future';
+    }
+    
+    if (midnightTimeInSeconds < timestampInSeconds) {
+        return evaluateHMS(timeDifference);
+    } else {
+        return evaluateYMWD((midnightTimeInSeconds - timestampInSeconds) + 24*60*60);
     }
 }
